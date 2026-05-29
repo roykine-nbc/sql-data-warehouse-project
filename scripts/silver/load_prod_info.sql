dc9c67638c4e24fc660f9100cd3826ff7e1260a3
roykine-nbc <92714174+roykine-nbc@users.noreply.github.com>
2026-05-29 15:18:38 +0700

    Add SQL script to load product info into silver table
    Insert product information into the silver.crm_prd_info table from bronze.crm_prd_info, including transformations for category ID and product line.

---
+++ scripts/silver/load_prod_info.sql
@@ -0,0 +1,17 @@
+insert into silver.crm_prd_info (prd_id,cat_id, prd_key,  prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
+select 
+prd_id,
+REPLACE(SUBSTRING(prd_key,1,5),'-','_') AS cat_id, -- Extract category ID
+SUBSTRING(prd_key,7,len(prd_key)) AS prd_key, -- Extract product key
+prd_nm,
+isnull(prd_cost,0) as prd_cost,
+case upper(trim(prd_line))
+	when 'M' then 'Mountain'
+	when 'R' then 'Road'
+	when 'S' then 'other Sales'
+	when 'T' then 'Touring'
+	else 'N/A'
+end as prd_line, -- Map product line codes to desciptive values
+prd_start_dt,
+dateadd(day,-1,lead(prd_start_dt) over(partition by prd_key order by prd_start_dt asc)) as prd_end_dt_test -- Calculate end date as one day before the next start date 
+from bronze.crm_prd_info 
